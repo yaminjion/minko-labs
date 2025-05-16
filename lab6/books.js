bd56ee84bb539bea70ae56d3689cf01c2ef541a6
Лена Минько <minkolena2005@mail.ru>
2025-05-16 19:37:46 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: "Лао-цзы",
+        name: "Дао дэ цзин. Канон о Дао и дэ",
+        publicationYear: 2022,
+        price: 850,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: "Карл Саган",
+        name: "Космос",
+        publicationYear: 2024,
+        price: 1200,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: "Баладжи Касал",
+        name: "Ошибки разумного инвестора",
+        publicationYear: 2025,
+        price: 950,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: "Джордж Оруэлл",
+        name: "1984",
+        publicationYear: 1949,
+        price: 600,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: "Рэй Брэдбери",
+        name: "451° по Фаренгейту",
+        publicationYear: 1953,
+        price: 550,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: "Фрэнсис Скотт Фицджеральд",
+        name: "Великий Гэтсби",
+        publicationYear: 1925,
+        price: 700,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: "Михаил Булгаков",
+        name: "Мастер и Маргарита",
+        publicationYear: 1967,
+        price: 800,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: "Эрих Мария Ремарк",
+        name: "Три товарища",
+        publicationYear: 1936,
+        price: 750,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: "Джером Д. Сэлинджер",
+        name: "Над пропастью во ржи",
+        publicationYear: 1951,
+        price: 650,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: "Антуан де Сент-Экзюпери",
+        name: "Маленький принц",
+        publicationYear: 1943,
+        price: 500,
+        bestseller: true,
+        inStock: true
+    }
+];
